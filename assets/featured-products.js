9e94111007203fc70613542bc2b192748e2dbdab
AlexDziunyk <alexdziunyk@gmail.com>
2025-04-12 21:04:00 +0300

    feat: hide products in section Featured Products if item already in the cart

--- assets/featured-products.js
+++ assets/featured-products.js
@@ -9,17 +9,31 @@ const addToCart = async (productId) => {
       ],
     };
 
-    const response = await fetch(window.Shopify.routes.root + "cart/add.js", {
+    await fetch(window.Shopify.routes.root + "cart/add.js", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(productData),
     });
-
-    const json = await response.json();
-    console.log("Item added!");
+    rerenderFeaturedProductsSection();
   } catch (error) {
     console.error(error);
+  }
+};
+
+const rerenderFeaturedProductsSection = async () => {
+  const response = await fetch(
+    window.Shopify.routes.root +
+      "?section_id=template--18418685313191__featured_products_Pd8eC9"
+  );
+  const html = await response.text();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/html");
+  const productsGrid = doc.querySelector(".featured-products__grid");
+  const currentGrid = document.querySelector(".featured-products__grid");
+
+  if (productsGrid && currentGrid) {
+    currentGrid.innerHTML = productsGrid.innerHTML;
   }
 };
