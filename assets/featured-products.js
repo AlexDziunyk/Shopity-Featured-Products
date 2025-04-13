ae167adcb9459222acc9b4f289c6ffe4093069fd
AlexDziunyk <alexdziunyk@gmail.com>
2025-04-13 18:58:52 +0300

    refactor: improved addToCart into web component

--- assets/featured-products.js
+++ assets/featured-products.js
@@ -1,39 +1,62 @@
-const addToCart = async (productId) => {
-  try {
-    const productData = {
-      items: [
-        {
-          id: productId,
-          quantity: 1,
+class FeaturedProductsSection extends HTMLElement {
+  constructor() {
+    super();
+    this.addToCart = this.addToCart.bind(this);
+  }
+
+  connectedCallback() {
+    this.addEventListener("click", this.addToCart);
+  }
+
+  disconnectedCallback() {
+    this.removeEventListener("click", this.addToCart);
+  }
+
+  async addToCart(event) {
+    try {
+      const button = event.target.closest(".featured-products__card-button");
+      if (!button) return;
+
+      const productId = button.dataset.productId;
+
+      const productData = {
+        items: [
+          {
+            id: parseInt(productId),
+            quantity: 1,
+          },
+        ],
+      };
+
+      await fetch(window.Shopify.routes.root + "cart/add.js", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-      ],
-    };
+        body: JSON.stringify(productData),
+      });
 
-    await fetch(window.Shopify.routes.root + "cart/add.js", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(productData),
-    });
-    rerenderFeaturedProductsSection();
-  } catch (error) {
-    console.error(error);
+      await this.rerenderFeaturedProductsSection();
+    } catch (error) {
+      console.error(error);
+    }
   }
-};
 
-const rerenderFeaturedProductsSection = async () => {
-  const response = await fetch(
-    window.Shopify.routes.root +
-      "?section_id=template--18418685313191__featured_products_Pd8eC9"
-  );
-  const html = await response.text();
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, "text/html");
-  const productsGrid = doc.querySelector(".featured-products__grid");
-  const currentGrid = document.querySelector(".featured-products__grid");
+  async rerenderFeaturedProductsSection() {
+    const response = await fetch(
+      window.Shopify.routes.root +
+        "?section_id=template--18418685313191__featured_products_YNKWeq"
+    );
+    const html = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    const productsGrid = doc.querySelector(".featured-products__grid");
+    const currentGrid = this.querySelector(".featured-products__grid");
 
-  if (productsGrid && currentGrid) {
-    currentGrid.innerHTML = productsGrid.innerHTML;
+    if (productsGrid && currentGrid) {
+      currentGrid.innerHTML = productsGrid.innerHTML;
+    }
   }
-};
+}
+
+customElements.define("featured-products-section", FeaturedProductsSection);
